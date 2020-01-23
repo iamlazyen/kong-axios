ff5ff9a0e0f5ca10e376fc92f7bc6ff6b5dcf7c3
depeng <kongdepeng520@gmail.com>
2020-01-23 11:48:32 +0800

    [test]: response headers

--- test/helpers/headers.test.ts
+++ test/helpers/headers.test.ts
@@ -1,4 +1,7 @@
-import { transformRequestHeaders } from '../../src/helpers/headers'
+import {
+  parseHeaders,
+  transformRequestHeaders
+} from '../../src/helpers/headers'
 
 describe('headers', function() {
   describe('headers:transformRequestHeaders', function() {
@@ -7,6 +10,25 @@ describe('headers', function() {
       expect(transformRequestHeaders({ a: 1 }, headers)).toEqual({
         'Content-type': 'application/json;charset=utf-8'
       })
+    })
+  })
+
+  describe('headers: parseHeaders', function() {
+    test('should parse headers', () => {
+      const parsed = parseHeaders(
+        'Content-Type: application/json\r\n' +
+          'Connection: keep-alive\r\n' +
+          'Transfer-Encoding: chunked\r\n' +
+          'Date: Tue, 21 May 2019 09:23:44 GMT\r\n' +
+          ':aa\r\n' +
+          'key:'
+      )
+      expect(parsed['content-type']).toBe('application/json')
+      expect(parsed['connection']).toBe('keep-alive')
+    })
+
+    test('parse headers is empty', () => {
+      expect(parseHeaders('')).toEqual({})
     })
   })
 })
